763e9f0b14258cae61c3779d979c341fbf0f7a8d
janverkade <jan.verkade@gmail.com>
2020-03-29 12:59:35 +0200

    changed project home folder

--- data_preparation/covid19_config.r
+++ data_preparation/covid19_config.r
@@ -1,4 +1,4 @@
 
 RDS_file_location <- "/home/jan/Dropbox/prive/covid19/"
-covid19_home <- "/home/jan/hobby-projecten/covid19/"
+covid19_home <- "/home/jan/covid19/"
 jhu_csv_folder <- "/home/jan/covid19_data/csse_covid_19_daily_reports/"
